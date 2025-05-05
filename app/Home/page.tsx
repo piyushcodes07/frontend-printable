adbb1d64f59ed7de27c2aacaaabf726edd9c49b8
piyushcodes07 <piyushcodes07@gmail.com>
2025-05-06 03:07:49 +0530

    merge doc conversion

---
+++ app/Home/page.tsx
@@ -0,0 +1,12 @@
+
+import Home_page from "@/components/home/page";
+export default function Home(){
+    return(
+        <>
+        <Home_page/>        
+        
+        
+        </>
+    )
+
+}
--- app/page.tsx
+++ app/page.tsx
@@ -12,3 +12,16 @@ export default function Home() {
 //         </>
 //     )
 // }
+
+ 
+// import Home_page from "@/components/home/page";
+// export default function Home(){
+//     return(
+//         <>
+//         <Home_page/>        
+        
+        
+//         </>
+//     )
+
+// }
